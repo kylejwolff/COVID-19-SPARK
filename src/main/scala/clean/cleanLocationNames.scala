53bef0b7e9081a6be4cf78d8c40bc3ebc9b142b7
brian <brja3022@colorado.edu>
2021-08-11 15:52:58 -0600

    Fixed order of columns

--- src/main/scala/clean/cleanLocationNames.scala
+++ src/main/scala/clean/cleanLocationNames.scala
@@ -26,37 +26,38 @@ object cleanLocationNames {
       .option("header", "true")
       .load("raw_data/covid_19_data.csv")
     println("Covid_19_data.csv read")
-    val dfP = df.withColumn("Province", when(col("Province/State").rlike("Diamond Princess"),
+    val dfP = df.withColumn("Province/State", when(col("Province/State").rlike("Diamond Princess"),
       "Diamond Princess").when(col("Province/State").rlike("Grand Princess"),
       "Grand Princess").when(col("Province/State")===col("Country/Region")||col("Province/State")==="None"||col("Province/State").rlike("Unknown"), null)
       .otherwise(trim(col("Province/State"))))
 
-    val dfProvince = dfP.drop(col("Province/State"))
 
-    val dfC = dfProvince.withColumn(
-      "Country",
+
+    val dfC = dfP.withColumn(
+      "Country/Region",
       when(
         col("Country/Region") === "('St. Martin',)",
         "St. Martin").when(col("Country/Region").rlike("Bahamas"),
         "Bahamas").when(col("Country/Region").rlike("Gambia"),
         "Gambia").otherwise(trim(col("Country/Region")))
     )
-    val dfCountry = dfC.drop(col("Country/Region"))
 
-    dfCountry.createOrReplaceTempView("covid_19_data")
-    spark.sql("SELECT * FROM covid_19_data LIMIT 5").show()
+
+    dfC.createOrReplaceTempView("covid_19_data")
+    spark.sql("SELECT * FROM covid_19_data").show()
 
     /*
         val conf = spark.read.format("csv")
           .option("inferSchema", "true")
           .option("header", "true")
-          .load("raw_data/time_series_covid_19_confirmed.csv")
+          .load("data/time_series_covid_19_confirmed.csv")
         conf.createOrReplaceTempView("confirmed")
         spark.sql("SELECT * FROM confirmed WHERE `Country/Region` LIKE '%South%'").show()
     */
+    spark.sql("SELECT * FROM covid_19_data WHERE `Country/Region` = 'Gambia'").show()
     //spark.sql("SELECT `Province`, count(`Province`), max(Confirmed) FROM covid_19_data group by `Province` order by count(`Province`) desc").show(250)
-    //spark.sql("SELECT `Country`, count(`Country`), max(Confirmed) FROM covid_19_data group by `Country` order by count(`Country`) desc").show(250)
-    dfCountry.write.format("com.databricks.spark.csv").mode(SaveMode.Overwrite).save("clean_data/covid_19_data.csv")
+    //spark.sql("SELECT `Country`, count(`Country`), max(Confirmed) FROM covid_19_data group by `Country` order by count(`Country`) asc").show(250)
+    dfC.write.format("com.databricks.spark.csv").mode(SaveMode.Overwrite).save("clean_data/covid_19_data.csv")
   }
 
 }
