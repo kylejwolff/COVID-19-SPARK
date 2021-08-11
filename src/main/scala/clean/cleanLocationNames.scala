a8595ff2882e6ea7023a23d45e9c7a2f31253722
brian <brja3022@colorado.edu>
2021-08-11 16:27:36 -0600

    Clean up code to be merged

--- src/main/scala/clean/cleanLocationNames.scala
+++ src/main/scala/clean/cleanLocationNames.scala
@@ -8,7 +8,7 @@ object cleanLocationNames {
   def main(args: Array[String]): Unit = {
     begin()
   }
-  def begin()={
+  def begin(): Unit={
     System.setProperty("hadoop.home.dir", "C:\\winutils")
 
     val spark = SparkSession
@@ -20,19 +20,18 @@ object cleanLocationNames {
     spark.sparkContext.setLogLevel("WARN")
 
 
-    val csvfile = "raw_data/covid_19_data.csv"
+    //Read data into dataframe
     val df = spark.read.format("csv")
       .option("inferSchema", "true")
       .option("header", "true")
       .load("raw_data/covid_19_data.csv")
-    println("Covid_19_data.csv read")
+    //println("Covid_19_data.csv read")
+    //Fix issues in province/state column
     val dfP = df.withColumn("Province/State", when(col("Province/State").rlike("Diamond Princess"),
       "Diamond Princess").when(col("Province/State").rlike("Grand Princess"),
       "Grand Princess").when(col("Province/State")===col("Country/Region")||col("Province/State")==="None"||col("Province/State").rlike("Unknown"), null)
       .otherwise(trim(col("Province/State"))))
-
-
-
+    //Fix issues in Country/Region column
     val dfC = dfP.withColumn(
       "Country/Region",
       when(
@@ -42,19 +41,12 @@ object cleanLocationNames {
         "Gambia").otherwise(trim(col("Country/Region")))
     )
 
-
+    //Move data into spark.sql view
     dfC.createOrReplaceTempView("covid_19_data")
-    spark.sql("SELECT * FROM covid_19_data").show()
+    //spark.sql("SELECT * FROM covid_19_data").show()
 
-    /*
-        val conf = spark.read.format("csv")
-          .option("inferSchema", "true")
-          .option("header", "true")
-          .load("data/time_series_covid_19_confirmed.csv")
-        conf.createOrReplaceTempView("confirmed")
-        spark.sql("SELECT * FROM confirmed WHERE `Country/Region` LIKE '%South%'").show()
-    */
-    spark.sql("SELECT * FROM covid_19_data WHERE `Country/Region` = 'Gambia'").show()
+
+    //spark.sql("SELECT * FROM covid_19_data WHERE `Country/Region` = 'Gambia'").show()
     //spark.sql("SELECT `Province`, count(`Province`), max(Confirmed) FROM covid_19_data group by `Province` order by count(`Province`) desc").show(250)
     //spark.sql("SELECT `Country`, count(`Country`), max(Confirmed) FROM covid_19_data group by `Country` order by count(`Country`) asc").show(250)
     dfC.write.format("com.databricks.spark.csv").mode(SaveMode.Overwrite).save("clean_data/covid_19_data.csv")
