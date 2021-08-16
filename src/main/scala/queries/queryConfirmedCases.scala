4e1128c7a90ce06ef3d5649f837b2298a6756f59
brian <brja3022@colorado.edu>
2021-08-16 13:18:01 -0600

    Write results to file

--- src/main/scala/queries/queryConfirmedCases.scala
+++ src/main/scala/queries/queryConfirmedCases.scala
@@ -2,7 +2,7 @@ package queries
 
 import org.apache.spark.sql
 import org.apache.spark.sql.{SparkSession, DataFrame}
-import tools.{Cleaner, Loader}
+import tools._
 
 object queryConfirmedCases {
   def main(args: Array[String]): Unit = {
@@ -16,20 +16,23 @@ object queryConfirmedCases {
     val uid_lookup_path = "raw_data/uid_lookup_table.csv"
     val uid_lookup = Cleaner.cleanUIDLookup(spark, Loader.loadCSV(spark, uid_lookup_path))
     val global_confirmed = Cleaner.cleanGlobalTimeSeries(spark, Loader.loadCSV(spark, global_confirmed_path))
-    //queryPerCapita(spark, global_confirmed, uid_lookup)
+    queryPerCapita(spark, global_confirmed, uid_lookup)
     queryTropical(spark, global_confirmed, uid_lookup)
   }
 
   def queryPerCapita(spark: SparkSession, global_confirmed: DataFrame, uid_lookup:DataFrame) =  {
     global_confirmed.createOrReplaceTempView("global_confirmed")
     uid_lookup.createOrReplaceTempView("uid_lookup")
-    global_confirmed.show()
+    //global_confirmed.show()
 
-    spark.sql("SELECT g.region, g.Country, (element_at(g.counts,(466))/u.Population) transmitionRate FROM global_confirmed g join uid_lookup u " +
-      "on g.region = u.region and g.Country = u.Country order by transmitionRate desc").show()
+    val regionRates = spark.sql("SELECT g.region, g.Country, (element_at(g.counts,(466))/u.Population) transmitionRate FROM global_confirmed g join uid_lookup u " +
+      "on g.region = u.region and g.Country = u.Country order by transmitionRate desc")
 
-    spark.sql("SELECT g.Country, (element_at(g.counts,466)/u.Population) transmitionRate FROM global_confirmed g join uid_lookup u " +
-      "on g.Country = u.Country where u.region is null order by transmitionRate desc").show()
+    val countryRates = spark.sql("SELECT g.Country, (element_at(g.counts,466)/u.Population) transmitionRate FROM global_confirmed g join uid_lookup u " +
+      "on g.Country = u.Country where u.region is null order by transmitionRate desc")
+    regionRates.show()
+    Writer.writeCSV(regionRates, "out/trans_by_region.csv", true)
+    Writer.writeCSV(countryRates, "out/trans_by_country.csv", true)
 
   }
 
@@ -37,10 +40,13 @@ object queryConfirmedCases {
     global_confirmed.createOrReplaceTempView("global_confirmed")
     uid_lookup.createOrReplaceTempView("uid_lookup")
 
-    spark.sql("SELECT (sum(element_at(g.counts,466))), (sum(u.Population)),(sum(element_at(g.counts,466))/sum(u.Population)) transmitionRate " +
-      "FROM global_confirmed g join uid_lookup u on g.latitude = u.latitude where u.latitude<23.5 and u.latitude>-23.5 order by transmitionRate desc").show()
-    spark.sql("SELECT (sum(element_at(g.counts,466))), (sum(u.Population)),(sum(element_at(g.counts,466))/sum(u.Population)) transmitionRate " +
-      "FROM global_confirmed g join uid_lookup u on g.latitude = u.latitude where u.latitude>23.5 or u.latitude<-23.5 order by transmitionRate desc").show()
+    val tropical = spark.sql("SELECT (sum(element_at(g.counts,466))), (sum(u.Population)),(sum(element_at(g.counts,466))/sum(u.Population)) transmitionRate " +
+      "FROM global_confirmed g join uid_lookup u on g.latitude = u.latitude where u.latitude<23.5 and u.latitude>-23.5 order by transmitionRate desc")
+    val nonTropical = spark.sql("SELECT (sum(element_at(g.counts,466))), (sum(u.Population)),(sum(element_at(g.counts,466))/sum(u.Population)) transmitionRate " +
+      "FROM global_confirmed g join uid_lookup u on g.latitude = u.latitude where u.latitude>23.5 or u.latitude<-23.5 order by transmitionRate desc")
+    Writer.writeCSV(tropical, "out/trans_tropical.csv", true)
+    Writer.writeCSV(nonTropical, "out/trans_nonTropical.csv", true)
+
   }
 
 }
